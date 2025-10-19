fdfc6710c0e5272570f1b4adf5ac3b62d5f3a5ce
unknown <bazaranova@gmail.com>
2025-10-19 19:21:25 +0500

    dz

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,16 +2,14 @@
 
 def solve():
 # Ниже пишите решение задачи
-    a, b, t = map(str, input().split())
-    z=(ord(a))
-    x=(ord(b))
-    c=(ord(t))
-    q=a 
-    w=b
-    e=t
-    print("Код символа " + str(q)+ "равен" + str(z) + "\n" "Код символа " + str(w)+ "равен" + str(x) + "\n" "Код символа " + str(e)+ "равен" + str(c))
-    
-   
+    a, b, c = map(str, input().split())
+    sim1 = a
+    sim2 = b
+    sim3 = c
+    code1 = ord(a)
+    code2 = ord(b)
+    code3 = ord(c)
+    print("Код символа " + str(sim1) + " равен " + str(code1) + "\n" + "Код символа " + str(sim2) + " равен " + str(code2) + "\n" + "Код символа " + str(sim3) + " равен " + str(code3))
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
